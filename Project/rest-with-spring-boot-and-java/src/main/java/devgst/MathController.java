4b4e44b45ab1b0269e7d772049cb8877312aa9c3
dev-gst <devgst@duck.com>
2024-01-01 21:15:05 -0300

    add exception handling

--- Project/rest-with-spring-boot-and-java/src/main/java/devgst/MathController.java
+++ Project/rest-with-spring-boot-and-java/src/main/java/devgst/MathController.java
@@ -2,23 +2,24 @@ package devgst;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import devgst.exceptions.UnsupportedMathOperationException;
 
 @RestController
 public class MathController {
 	private final AtomicLong  counter = new AtomicLong();
 	
-	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}",
-			method = RequestMethod.GET)
+	@GetMapping(value = "/sum/{numberOne}/{numberTwo}")
 	public Double sum (
 			@PathVariable(value = "numberOne") String numberOne,
 			@PathVariable(value = "numberTwo") String numberTwo
 			) throws Exception {
+		
 		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-			throw new Exception();
+			throw new UnsupportedMathOperationException("Please set a numeric value.");
 		}
 		
 		return convertToDouble(numberOne) + convertToDouble(numberTwo);
